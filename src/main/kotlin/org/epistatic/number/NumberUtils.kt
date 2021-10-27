63ff6710155c52e94961aa7ff03b86225633d934
Serge Merzliakov <smerzlia@gmail.com>
2021-10-27 11:32:24 +1100

    Added integer and real number input for TextFields

--- src/main/kotlin/org/epistatic/number/NumberUtils.kt
+++ src/main/kotlin/org/epistatic/number/NumberUtils.kt
@@ -1,7 +1,14 @@
 package org.epistatic.number
 
+import javafx.scene.control.TextField
+import javafx.scene.control.TextFormatter
+import java.text.DecimalFormat
+import java.text.ParsePosition
 import kotlin.math.pow
 import kotlin.math.roundToInt
+
+
+private val integerFormat = DecimalFormat("#")
 
 
 fun Double.roundTo(numFractionDigits: Int): Double {
@@ -13,3 +20,36 @@ fun Float.roundTo(numFractionDigits: Int): Float {
     val factor = 10.0f.pow(numFractionDigits.toFloat())
     return (this * factor).roundToInt() / factor
 }
+
+
+fun acceptIntegers(field: TextField) {
+    field.textFormatter = TextFormatter<Any> { c: TextFormatter.Change ->
+        if (c.controlNewText.isEmpty()) {
+            c
+        }
+        integerFormat.isParseIntegerOnly = true
+        val parsePosition = ParsePosition(0)
+        val obj = integerFormat.parse(c.controlNewText, parsePosition)
+        if (obj == null || parsePosition.index < c.controlNewText.length) {
+            null // not a digit
+        } else {
+            c
+        }
+    }
+}
+
+fun acceptDoubles(field: TextField) {
+    field.textFormatter = TextFormatter<Any> { c: TextFormatter.Change ->
+        if (c.controlNewText.isEmpty()) {
+            c
+        }
+        val parsePosition = ParsePosition(0)
+        val obj = integerFormat.parse(c.controlNewText, parsePosition)
+        if (obj == null || parsePosition.index < c.controlNewText.length) {
+            null // not a digit
+        } else {
+            c
+        }
+    }
+}
+
