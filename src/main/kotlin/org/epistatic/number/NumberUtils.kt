9c8bb5bab792f619a07a58249c3648daa654f843
Serge Merzliakov <smerzlia@gmail.com>
2021-12-15 21:02:37 +1100

    Revert "Upgraded to apple silicon and log4j 2.15"
    This reverts commit 4e913605e89d0e680d66122d729d7485c7a16793.

--- src/main/kotlin/org/epistatic/number/NumberUtils.kt
+++ src/main/kotlin/org/epistatic/number/NumberUtils.kt
@@ -43,7 +43,6 @@ fun acceptDoubles(field: TextField) {
         if (c.controlNewText.isEmpty()) {
             c
         }
-        integerFormat.isParseIntegerOnly = false
         val parsePosition = ParsePosition(0)
         val obj = integerFormat.parse(c.controlNewText, parsePosition)
         if (obj == null || parsePosition.index < c.controlNewText.length) {
