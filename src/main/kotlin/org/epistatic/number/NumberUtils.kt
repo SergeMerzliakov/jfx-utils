14915cdf2cc3795fc0d785506e72735e7e371b87
Serge Merzliakov <smerzlia@gmail.com>
2022-02-18 11:01:38 +1100

    better comments, clean up stale dependencies

--- src/main/kotlin/org/epistatic/number/NumberUtils.kt
+++ src/main/kotlin/org/epistatic/number/NumberUtils.kt
@@ -7,20 +7,19 @@ import java.text.ParsePosition
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
-
 private val integerFormat = DecimalFormat("#")
 
-
+// TODO fix - does not work for large values
 fun Double.roundTo(numFractionDigits: Int): Double {
     val factor = 10.0.pow(numFractionDigits.toDouble())
     return (this * factor).roundToInt() / factor
 }
 
+// TODO fix - does not work for large values
 fun Float.roundTo(numFractionDigits: Int): Float {
     val factor = 10.0f.pow(numFractionDigits.toFloat())
     return (this * factor).roundToInt() / factor
 }
-
 
 fun acceptIntegers(field: TextField) {
     field.textFormatter = TextFormatter<Any> { c: TextFormatter.Change ->
