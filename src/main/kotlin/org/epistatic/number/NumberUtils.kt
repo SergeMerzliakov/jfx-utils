84fb022b0beb2b492994fac70d8980b82469862f
Serge Merzliakov <smerzlia@gmail.com>
2024-05-05 10:53:28 +1000

    updated to latest dependencies, JDK 21 and JFX 21

--- src/main/kotlin/org/epistatic/number/NumberUtils.kt
+++ src/main/kotlin/org/epistatic/number/NumberUtils.kt
@@ -22,32 +22,32 @@ fun Float.roundTo(numFractionDigits: Int): Float {
 }
 
 fun acceptIntegers(field: TextField) {
-    field.textFormatter = TextFormatter<Any> { c: TextFormatter.Change ->
-        if (c.controlNewText.isEmpty()) {
-            c
+    field.textFormatter = TextFormatter<Any> { ch: TextFormatter.Change ->
+        if (ch.controlNewText.isEmpty()) {
+            ch
         }
         integerFormat.isParseIntegerOnly = true
         val parsePosition = ParsePosition(0)
-        val obj = integerFormat.parse(c.controlNewText, parsePosition)
-        if (obj == null || parsePosition.index < c.controlNewText.length) {
+        val obj = integerFormat.parse(ch.controlNewText, parsePosition)
+        if (obj == null || parsePosition.index < ch.controlNewText.length) {
             null // not a digit
         } else {
-            c
+            ch
         }
     }
 }
 
 fun acceptDoubles(field: TextField) {
-    field.textFormatter = TextFormatter<Any> { c: TextFormatter.Change ->
-        if (c.controlNewText.isEmpty()) {
-            c
+    field.textFormatter = TextFormatter<Any> { ch: TextFormatter.Change ->
+        if (ch.controlNewText.isEmpty()) {
+            ch
         }
         val parsePosition = ParsePosition(0)
-        val obj = integerFormat.parse(c.controlNewText, parsePosition)
-        if (obj == null || parsePosition.index < c.controlNewText.length) {
+        val obj = integerFormat.parse(ch.controlNewText, parsePosition)
+        if (obj == null || parsePosition.index < ch.controlNewText.length) {
             null // not a digit
         } else {
-            c
+            ch
         }
     }
 }
